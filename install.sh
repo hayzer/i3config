79ffb09e1f0253886c1b705b8ab708e930624d09
Thomas Maier <hayzer@gmail.com>
2014-02-12 16:03:49 +0100

    Testing version of install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
-set -o nounset                              # Treat unset variables as an error
+set -o nounset
 
-echo "Work!"
+home=${HOME}
+source=i3config.tar.gz
+
+(
+	cd /tmp
+	curl -s -L -o ${source} http://github.com/hayzer/i3config/tarball/master
+	tar zxvf ${source}
+	cd hayzer-*/
+	mkdir ${home}/tmp/.i3
+	cp dot-i3-config ${home}/tmp/.i3/config
+	cp dot-i3status.conf ${home}/tmp/.i3status.conf
+	cd -
+	rm -fr hayzer-*/ ${source}
+)
+
+
