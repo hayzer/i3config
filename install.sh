45056f62542146ee53dee1e39362d8f1b1966336
Thomas Maier <hayzer@gmail.com>
2014-02-12 23:22:14 +0100

    Update and fix installation

--- install.sh
+++ install.sh
@@ -5,18 +5,20 @@ source=i3config.tar.gz
 
 (
 	cd /tmp
+	mkdir -p ${home}/.i3
 	curl -s -L -o ${source} http://github.com/hayzer/i3config/tarball/master
 	tar zxvf ${source}
 	cd hayzer-*/
-	mkdir ${home}/.i3
 
-	if ${I3WORK}; then
+	if test ${I3WORK-}; then
 		cp dot-i3-config-work ${home}/.i3/config
+		cp dot-i3status.conf-work ${home}/.i3status.conf
 		# I assume ~/bin is in PATH
 		mkdir -p ${home}/bin
 		cp work-i3status.sh ${home}/bin
 	else
 		cp dot-i3-config ${home}/.i3/config
+		cp dot-i3status.conf ${home}/.i3status.conf
 	fi
 	cp dot-i3status.conf ${home}/.i3status.conf
 	rm -fr hayzer-*/ ${source}
