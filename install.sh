ffe64a6156c23f400440187a6e4e1043463d6249
Thomas Maier <hayzer@gmail.com>
2014-02-13 09:39:44 +0100

    Add Xdefaults to the installer

--- install.sh
+++ install.sh
@@ -3,21 +3,22 @@ source=i3config.tar.gz
 
 (
 	cd /tmp
-	mkdir -p ${home}/.i3
 	curl -s -L -o ${source} http://github.com/hayzer/i3config/tarball/master
 	tar zxf ${source}
 	cd hayzer-*/
+	mkdir -p ${home}/.i3
 
 	if test ${I3WORK-}; then
-		cp dot-i3-config-work ${home}/.i3/config
-		cp dot-i3status.conf-work ${home}/.i3status.conf
-		# I assume ~/bin is in PATH
 		mkdir -p ${home}/bin
-		cp work-i3status.sh ${home}/bin
+		cp dot-i3-config-work     ${home}/.i3/config
+		cp dot-i3status.conf-work ${home}/.i3status.conf
+		cp work-i3status.sh       ${home}/bin
 	else
-		cp dot-i3-config ${home}/.i3/config
+		cp dot-i3-config     ${home}/.i3/config
 		cp dot-i3status.conf ${home}/.i3status.conf
 	fi
+	cp dot-xdefaults ${home}/.Xdefaults
+	cd -
 	rm -fr hayzer-*/ ${source}
 )
 
